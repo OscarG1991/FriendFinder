a547d2b0a42cd5e0a1954ba8da3b8041f17065cb
Oscar <Oscarg_1991@yahoo.com>
2019-01-27 23:08:27 -0700

    update files

---
+++ data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+  {
+  name:"Ahmed",
+  photo:"http://www.sunny-cat.ru/datas/users/1-funny-star-sunray-017.jpg",
+  scores:[
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
