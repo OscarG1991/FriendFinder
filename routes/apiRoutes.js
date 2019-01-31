837e54f3ca0b39f2761287f4baaab9db5acdbfd5
Oscar <Oscarg_1991@yahoo.com>
2019-01-30 19:11:33 -0700

    all files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,14 +8,51 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+  app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		// console.log('userInput = ' + JSON.stringify(userInput));
 
-app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body);
-      res.json(true);
-  
-  });
-  };
+		var userResponses = userInput.scores;
+		// console.log('userResponses = ' + userResponses);
+
+		// Compute best friend match
+		var name = '';
+		var photo = '';
+		var totalDifference = 10000; // Make the initial value big for comparison
+
+		// Examine all existing friends in the list
+		for (var i = 0; i < friendsData.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friendsData[i].scores[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				// console.log('Closest match found = ' + diff);
+				// console.log('Friend name = ' + friends[i].name);
+				// console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				name = friendsData[i].name;
+				photo = friendsData[i].photo;
+			}
+		}
+
+		// Add new user
+		friendsData.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', name: name, photo: photo});
+	});
+};
