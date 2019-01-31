568831545448ee3be4c7b54bd5dff7d6f3988e67
Oscar <Oscarg_1991@yahoo.com>
2019-01-30 19:19:42 -0700

    friendsData api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get('/api/friends', function(req, res) {
-		res.json(friends);
+		res.json(friendsData);
 	});
 
 	// Add new friend entry
